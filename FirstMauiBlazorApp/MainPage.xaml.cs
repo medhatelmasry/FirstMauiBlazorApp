1c45cd5f9f2d16c6e6db5c03ec3b881b330a9d9b
Medhat Elmasry <medhat@elmasry.ca>
2021-12-17 14:18:01 -0800

    1st commit

---
+++ FirstMauiBlazorApp/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FirstMauiBlazorApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ FirstMauiBlazorApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace FirstMauiBlazorApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
